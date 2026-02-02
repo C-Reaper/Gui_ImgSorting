ad3a1e8bf455028c360513d7f844ce35c4f266d2
codeleaded <gnu.c.reaper@gmail.com>
2026-02-02 15:47:19 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -4,55 +4,39 @@
 
 Sprite input;
 Sprite sorted;
+Vec2 pos;
 
 int Sprite_Sort(const Pixel* sp0,const Pixel* sp1){
-	return Pixel_Lightness_RGB(*sp0) > Pixel_Lightness_RGB(*sp1);
+	const float l0 = Pixel_Lightness_RGB(*sp0);
+	const float l1 = Pixel_Lightness_RGB(*sp1);
+	return l0 > l1;
 }
 
 void Setup(AlxWindow* w){
-	
+	input = Sprite_Load("./assets/Background.png");
+	sorted = Sprite_Cpy(&input);
+	pos = (Vec2){ 0.0f,0.0f };
 }
 void Update(AlxWindow* w){
-    if(Stroke(ALX_KEY_W).DOWN){
-		speed *= 1.01f;
-	}
-	if(Stroke(ALX_KEY_S).DOWN){
-		speed *= 0.99f;
+    if(Stroke(ALX_MOUSE_L).DOWN){
+		pos = GetMouse();
+
+		Sprite_Free(&sorted);
+		sorted = Sprite_Cpy(&input);
+		qsort(sorted.img,sorted.w * sorted.h,sizeof(Pixel),Sprite_Sort);
 	}
 	
 	Clear(BLACK);
 
-	for(int i = 0;i<stars.size;i++){
-		Vec3* p = (Vec3*)Vector_Get(&stars,i);
-		const Vec2 pp = World_Screen(*p);
-
-		p->z -= speed * w->ElapsedTime;
-		if(p->z < 0.01f){
-			Vector_Remove(&stars,i);
-			i--;
-			continue;
-		}
-
-		const Vec2 ap = World_Screen(*p);
-		Line_RenderX(WINDOW_STD_ARGS,pp,ap,WHITE,1.0f);
-		//Circle_RenderX(WINDOW_STD_ARGS,World_Screen(*p),1.0f,WHITE);
-	}
-
-
-	for(int i = stars.size;i<STARS_MAX;i++){
-		Vector_Push(&stars,(Vec3[]){{
-			.x = Random_f64_MinMax(-10.0f,10.0f),
-			.y = Random_f64_MinMax(-10.0f,10.0f),
-			.z = Random_f64_MinMax(5.0f,15.0f)
-		}});
-	}
+	Sprite_Render(WINDOW_STD_ARGS,&input,0.0f,0.0f);
+	Sprite_Render(WINDOW_STD_ARGS,&sorted,input.w,0.0f);
 }
 void Delete(AlxWindow* w){
-    Vector_Free(&stars);
+    
 }
 
 int main(){
-    if(Create("Star Field",1920,1080,1,1,Setup,Update,Delete))
+    if(Create("Img Sorting",1920,1080,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
